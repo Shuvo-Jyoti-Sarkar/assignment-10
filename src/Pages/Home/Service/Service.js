6ac04f42a8720969cbe11d7ca65b59d2a017d6b7
Shuvo-Jyoti-Sarkar <shuvojyoti98@gmail.com>
2022-04-17 23:19:51 +0600

    service section is created

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Service.css';
 
 const Service = ({ service }) => {
     const { name, price, img, description } = service;
 
     return (
-        <div>
+        <div className='service'>
             <img src={img} alt="" />
             <h2> {name}</h2>
             <p>Price: {price}</p>
